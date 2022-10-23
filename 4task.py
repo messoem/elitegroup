8a6c9840cfa1ecdcc2b8a2d5df0022fff91ec5ed
reaper666 <57996364+novichok228@users.noreply.github.com>
2022-10-23 14:53:56 +0300

    Create 4task.py

---
+++ 4task.py
@@ -0,0 +1,5 @@
+listofnames = ['Vasya', 'Nikolay', 'Dmitriy']
+listofadreses = ['192.168.8.1', '201.255.11.1', '186.109.1.1']
+listoflogins = ['v@sy@1337', 'K0lya_666', 'Dima_Dima']
+listofusers = [{'name':listofnames[i], 'ip':listofadreses[i], 'login':listoflogins[i]} for i in range(3)]
+print(listofusers)
